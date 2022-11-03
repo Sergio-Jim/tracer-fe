2e4d7a6004c5cec40cbc82539ab550eeb242d14c
Jimmy Sergio <jimmy1sergio2@gmail.com>
2022-11-03 14:17:58 +0200

    login message

--- src/components/loginProcess.vue
+++ src/components/loginProcess.vue
@@ -112,8 +112,10 @@
 
       <div id="enter_otp" style="display: none">
         <form form v-on:submit.prevent="verifyOtp">
-          <span>A PIN has been sent to {{ this.phone_number }}</span>
-          <span>Please enter that PIN to login</span>
+          <div class="flex flex-col items-center">
+            <span>A PIN has been sent to {{ this.phone_number }}</span>
+            <span>Please enter that PIN to login</span>
+          </div>
           <div style="margin: 30px 0">
             <input
               placeholder="PIN"
