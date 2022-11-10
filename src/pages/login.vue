4515dd696d5270402b29da8238dbc7107e1370f1
Jimmy Sergio <jimmy1sergio2@gmail.com>
2022-11-10 02:16:18 +0200

    login view

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -14,7 +14,7 @@
         flex-grow: 1;
         display: flex;
         flex-direction: row;
-        height: 100vh;
+        height: 100%;
         width: 100%;
         align-items: center;
         overflow-y: scroll;
@@ -49,7 +49,7 @@
     </div>
     <div
       class="mb-52"
-      style="display: flex; flex-direction: column; width: 100%; height: 100vh"
+      style="display: flex; flex-direction: column; width: 100%; height: 100%"
     >
       <div
         style="
